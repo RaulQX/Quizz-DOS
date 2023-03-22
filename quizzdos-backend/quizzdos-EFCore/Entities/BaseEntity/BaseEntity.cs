6c08fadb7709715aa3955c21cf43c3d2e5346b74
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-03-22 21:30:28 +0200

    27 - Base Entity

---
+++ quizzdos-backend/quizzdos-EFCore/Entities/BaseEntity/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizzdos_EFCore.Entities.BaseEntity
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
