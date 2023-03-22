92a1d6c2c6f9f8f2b014278cb93ff2c2013c431b
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-03-22 21:29:48 +0200

    28 - backend enums

---
+++ quizzdos-backend/quizzdos-EFCore/Enums/QuestionTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizzdos_EFCore.Enums
+{
+    public enum QuestionType
+    {
+        SingleChoice = 0,
+        MultipleChoice = 1
+    }
+}
---
+++ quizzdos-backend/quizzdos-EFCore/Enums/RoleEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizzdos_EFCore.Enums
+{
+    public enum ERole
+    {
+        Student = 0,
+        Professor = 1,
+        Admin = 2
+    }
+}
