be01db2aea27c5af61368ac6668d94a5ff4aa6c4
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-14 17:52:53 +0300

    Update App.tsx

--- quizzdos-frontend/App.tsx
+++ quizzdos-frontend/App.tsx
@@ -1,15 +1,18 @@
-import { createStackNavigator } from "@react-navigation/stack"
 import React from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+import { StatusBar } from "expo-status-bar"
 import { StyleSheet } from "react-native"
-import Welcome from "./screens/common/Welcome"
 import { NavigationContainer, DarkTheme } from "@react-navigation/native"
 import { COLORS } from "./palette/colors"
+import Welcome from "./screens/common/Welcome"
 import Login from "screens/common/Login"
 import SignUp from "screens/common/SignUp"
 import FirstTimeSignIn from "screens/common/FirstTimeSignIn"
 import Course from "./screens/Student/Course"
-import { StatusBar } from "expo-status-bar"
 import StudentHome from "screens/Student/StudentHome"
+import StudentStatistics from "screens/Student/StudentStatistics"
+import Notifications from "screens/common/Notifications"
+import QuizStart from "screens/Student/QuizStart"
 
 const Stack = createStackNavigator()
 
@@ -23,8 +26,17 @@ export default function App() {
 					cardStyle: styles.cardStyle,
 				}}
 			>
-				<Stack.Screen name="StudentHome" component={StudentHome} />
+				<Stack.Screen name="QuizStart" component={QuizStart} />
+
 				<Stack.Screen name="Course" component={Course} />
+				<Stack.Screen name="Notifications" component={Notifications} />
+				<Stack.Screen
+					name="StudentStatistics"
+					component={StudentStatistics}
+				/>
+
+				<Stack.Screen name="StudentHome" component={StudentHome} />
+
 				<Stack.Screen
 					name="FirstTimeSignIn"
 					component={FirstTimeSignIn}
