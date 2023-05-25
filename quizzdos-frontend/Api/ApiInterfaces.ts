f554531aa915da1cb855f485fc7004c8eecc9d98
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-25 16:04:32 +0300

    Update ApiInterfaces.ts

--- quizzdos-frontend/Api/ApiInterfaces.ts
+++ quizzdos-frontend/Api/ApiInterfaces.ts
@@ -1,6 +1,16 @@
+
 export interface User {
 	username: string
 	email: string
 	phoneNumber: string
 	password: string
+}
+
+export type EGender = 0 | 1 | 2
+export type PRole = 0 | 1 | 2
+
+export interface NewAccountDetails {
+	firstName: string
+	lastName: string
+	gender: EGender
 }
