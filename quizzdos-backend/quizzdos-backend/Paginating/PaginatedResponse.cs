e4c8a8a2452c507b0ac574bc46f76f818f8ea890
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-25 16:02:44 +0300

    Create PaginatedResponse.cs

---
+++ quizzdos-backend/quizzdos-backend/Paginating/PaginatedResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace quizzdos_backend.Paging
+{
+    public class PaginatedResponse<T>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        public IEnumerable<T> Data { get; set; }
+        public PaginatedResponse(int page, int pageSize, int totalCount, IEnumerable<T> data)
+        {
+            Page = page;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+            Data = data;
+        }
+    }
+}
