f42c27ac45caaa95ef31334baae19a392392c297
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-25 16:04:52 +0300

    Update ApiConstants.ts

--- quizzdos-frontend/constants/ApiConstants.ts
+++ quizzdos-frontend/constants/ApiConstants.ts
@@ -9,11 +9,20 @@ const baseUrl = "http://192.168.1.169:5000/api"
 
 const controllers = {
 	auth: makeRoute(baseUrl, "auth"),
+	user: makeRoute(baseUrl, "user"),
+	person: makeRoute(baseUrl, "person"),
 } as const
 
 export const ApiEndpoints = {
 	Auth: {
 		login: makeRoute(controllers.auth, "login"),
 		register: makeRoute(controllers.auth, "register"),
+	},
+	Person: {
+		personByUserId: makeRoute(controllers.person, "userId"),
+		personById: controllers.person,
+	},
+	User: {
+		currentUser: makeRoute(controllers.user, "current-user"),
 	},
 } as const
