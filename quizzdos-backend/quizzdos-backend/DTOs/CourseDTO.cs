78e6be78552f8eb2a5df3b8503959215c2c5beae
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-25 16:02:37 +0300

    Create CourseDTO.cs

---
+++ quizzdos-backend/quizzdos-backend/DTOs/CourseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace quizzdos_backend.DTOs
+{
+    public class CourseDTO
+    {
+        public Guid CreatorId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ShortName { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string MaterialsUrl { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+    }
+}
