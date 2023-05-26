aa10984b9ab82455118e4461f40f94fc6a83d9e8
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-26 15:39:45 +0300

    Update CourseDTO.cs

--- quizzdos-backend/quizzdos-backend/DTOs/CourseDTO.cs
+++ quizzdos-backend/quizzdos-backend/DTOs/CourseDTO.cs
@@ -9,4 +9,14 @@
         public string MaterialsUrl { get; set; } = string.Empty;
         public string Icon { get; set; } = string.Empty;
     }
+
+    public class DiplayCourseDTO
+    {
+        public Guid Id { get; set; }
+        public string ShortName { get; set; } = string.Empty;
+        public int SectionsNumber { get; set; }
+        public double Progress { get; set; }
+        public string? Icon { get; set; } = string.Empty;
+        public string? Code { get; set; } = string.Empty;
+    }
 }
