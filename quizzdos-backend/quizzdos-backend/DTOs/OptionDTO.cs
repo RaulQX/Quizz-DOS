f4926884f6dfe8ed0c25784bf558815b2fb26719
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-05-30 16:05:58 +0300

    added dtos

---
+++ quizzdos-backend/quizzdos-backend/DTOs/OptionDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace quizzdos_backend.DTOs
+{
+    public class OptionDTO
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public double ScorePercentage { get; set; } 
+    }
+
+    public class QuestionOptionDTO
+    {
+        public string Text { get; set; } = string.Empty;
+        public double ScorePercentage { get; set; }
+
+    }
+}
---
+++ quizzdos-backend/quizzdos-backend/DTOs/QuestionDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace quizzdos_backend.DTOs
+{
+    public class QuestionDTO
+    {
+        public Guid Id { get; set; }
+        public string Prompt { get; set; } = string.Empty;
+        public double QuestionScore { get; set; }
+        public bool TipAllowed { get; set; } = false;
+        public List<OptionDTO> Options { get; set; } = new List<OptionDTO>();
+    }
+
+    public class QuizQuestionDTO
+    {
+        public Guid QuizId { get; set; }
+        public Guid Id { get; set; }
+        public string Prompt { get; set; } = string.Empty;
+        public double QuestionScore { get; set; }
+        public bool TipAllowed { get; set; }
+        public List<QuestionOptionDTO> Options { get; set;  } = new List<QuestionOptionDTO>();
+    }
+
+}
